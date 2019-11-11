ca3c0bbff6e0e20c68228e0a5f927d4f6a2a2388
Mohamed Emir Jammeli <mohamedemir.jammeli@advyteam.com>
2019-11-11 15:45:17 +0100

    modif api-gateway

--- api-gateway/src/main/java/com/esprit/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/esprit/apigateway/ApiGatewayApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableFeignClients
 @EnableCircuitBreaker
 @SpringBootApplication
 public class ApiGatewayApplication {
